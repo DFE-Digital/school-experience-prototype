df79d6df187719c29b7c9202925bd53cef978a78
Neil Scales <neil.scales@outlook.com>
2018-10-02 16:23:28 +0100

    Filter schools based on search criteria

--- app/filters.js
+++ app/filters.js
@@ -1,45 +1,59 @@
 module.exports = function (env) {
-  /**
-   * Instantiate object used to store the methods registered as a
-   * 'filter' (of the same name) within nunjucks. You can override
-   * gov.uk core filters by creating filter methods of the same name.
-   * @type {Object}
-   */
-  var filters = {}
+    /**
+     * Instantiate object used to store the methods registered as a
+     * 'filter' (of the same name) within nunjucks. You can override
+     * gov.uk core filters by creating filter methods of the same name.
+     * @type {Object}
+     */
+    var filters = {};
 
-  /* ------------------------------------------------------------------
-    add your methods to the filters obj below this comment block:
-    @example:
+    /* ------------------------------------------------------------------
+      add your methods to the filters obj below this comment block:
+      @example:
+  
+      filters.sayHi = function(name) {
+          return 'Hi ' + name + '!'
+      }
+  
+      Which in your templates would be used as:
+  
+      {{ 'Paul' | sayHi }} => 'Hi Paul'
+  
+      Notice the first argument of your filters method is whatever
+      gets 'piped' via '|' to the filter.
+  
+      Filters can take additional arguments, for example:
+  
+      filters.sayHi = function(name,tone) {
+        return (tone == 'formal' ? 'Greetings' : 'Hi') + ' ' + name + '!'
+      }
+  
+      Which would be used like this:
+  
+      {{ 'Joel' | sayHi('formal') }} => 'Greetings Joel!'
+      {{ 'Gemma' | sayHi }} => 'Hi Gemma!'
+  
+      For more on filters and how to write them see the Nunjucks
+      documentation.
+  
+    ------------------------------------------------------------------ */
 
-    filters.sayHi = function(name) {
-        return 'Hi ' + name + '!'
-    }
+    env.addFilter('schoolFilter', function (items, distance, criteria, searchType) {
+        criteria = criteria.toUpperCase();
 
-    Which in your templates would be used as:
+        switch (searchType) {
+            case 'isLocationSearch':
+                return items.filter(function (item, index) { return item.Distance <= distance; });
+            case 'isSubjectAndSchoolTypeSearch':
+                return items.filter(function (item, index) { return (item.Subjects.toUpperCase().includes(criteria) || item.SchoolType.toUpperCase().includes(criteria)) && item.Distance <= distance; });
+            case 'isSubjectAndLocationSearch':
+                return items.filter(function (item, index) { return item.Subjects.toUpperCase().includes(criteria) && item.Distance <= distance; });
+        }
+        return items;
+    });
 
-    {{ 'Paul' | sayHi }} => 'Hi Paul'
-
-    Notice the first argument of your filters method is whatever
-    gets 'piped' via '|' to the filter.
-
-    Filters can take additional arguments, for example:
-
-    filters.sayHi = function(name,tone) {
-      return (tone == 'formal' ? 'Greetings' : 'Hi') + ' ' + name + '!'
-    }
-
-    Which would be used like this:
-
-    {{ 'Joel' | sayHi('formal') }} => 'Greetings Joel!'
-    {{ 'Gemma' | sayHi }} => 'Hi Gemma!'
-
-    For more on filters and how to write them see the Nunjucks
-    documentation.
-
-  ------------------------------------------------------------------ */
-
-  /* ------------------------------------------------------------------
-    keep the following line to return your filters to the app
-  ------------------------------------------------------------------ */
-  return filters
-}
+    /* ------------------------------------------------------------------
+      keep the following line to return your filters to the app
+    ------------------------------------------------------------------ */
+    return filters;
+};
