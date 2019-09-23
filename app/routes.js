b583fa883e1232383cb9d0538deefba5e4002927
Peter Yates <peter.yates@graphia.co.uk>
2019-09-23 15:54:04 +0100

    Copy handler for schools-edit-dates-added

--- app/routes.js
+++ app/routes.js
@@ -1287,6 +1287,16 @@ router.post('/schools/availability-start-dates-edit', function(req, res) {
   }
 }) ;
 
+router.post('/schools/school-edit-dates-added', function(req, res) {
+  if (req.session.data['all-subjects'] == 'yes') {
+    let dates = [ { date: '5 September 2019', duration: 2, subjects: 'All', status: 'AVAILABLE', tag: 'available' } ];
+    let q = JSON.stringify(dates);
+    res.redirect('/schools/school-edit-dates' + '?json=' + q) ;
+  } else {
+    res.redirect('/schools/availability-select-dates-subjects-all') ;
+  }
+});
+
 router.get('/schools/school-edit-dates', function(req, res) {
   let dates;
 
