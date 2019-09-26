646903da6715ff90983bf918a0ba4e7e71b36eda
Paul Ripley <ripleydesign@gmail.com>
2019-09-26 11:59:09 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Get school experience',
+  serviceName: 'Manage school experience',
 
   // Default port that prototype runs on
   port: '3001',
